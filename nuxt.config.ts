0c8b4786a771eac7be6c3b8b68fa56a60683298e
Fariz Akbar <77776418+Fariz313@users.noreply.github.com>
2024-05-05 14:44:56 +0700

    initialize

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+    ,'@nuxtjs/color-mode'
+  ],
+  colorMode: {
+    classSuffix: ''
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="h-screen pt-40">
+        <div class="container mx-auto">
+            <SliderCard :data="data">
+
+            </SliderCard>
+            <!-- <div class="grid grid-cols-4 gap-4">
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">01</div>
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">02</div>
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">03</div>
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">04</div>
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">05</div>
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">06</div>
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">07</div>
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">08</div>
+                <div class="bg-slate-700 h-20 -skew-x-[20deg]">09</div>
+            </div> -->
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            data: [
+                {
+                    "title": "Mobile Legend",
+                    "platform": ["Windows", "Android"],
+                    "image": "https://cdn0-production-images-kly.akamaized.net/Hyl2S_OHDEDCGayRTeriz3hPi38=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3488932/original/008102000_1624278462-Mobile_Legends_11.jpg"
+                },
+                {
+                    "title": "Mobile Legend",
+                    "platform": ["Windows", "Android"],
+                    "image": "https://cdn0-production-images-kly.akamaized.net/Hyl2S_OHDEDCGayRTeriz3hPi38=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3488932/original/008102000_1624278462-Mobile_Legends_11.jpg"
+                },
+                {
+                    "title": "Mobile Legend",
+                    "platform": ["Windows", "Android"],
+                    "image": "https://cdn0-production-images-kly.akamaized.net/Hyl2S_OHDEDCGayRTeriz3hPi38=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3488932/original/008102000_1624278462-Mobile_Legends_11.jpg"
+                },
+                {
+                    "title": "Mobile Legend",
+                    "platform": ["Windows", "Android"],
+                    "image": "https://cdn0-production-images-kly.akamaized.net/Hyl2S_OHDEDCGayRTeriz3hPi38=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3488932/original/008102000_1624278462-Mobile_Legends_11.jpg"
+                },
+                {
+                    "title": "Mobile Legend",
+                    "platform": ["Windows", "Android"],
+                    "image": "https://cdn0-production-images-kly.akamaized.net/Hyl2S_OHDEDCGayRTeriz3hPi38=/1200x675/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3488932/original/008102000_1624278462-Mobile_Legends_11.jpg"
+                },
+            ]
+        }
+    }
+}
+</script>
